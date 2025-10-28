1c9ab33744d656ebca8bb07290c44fd36dbd3fce
elliesbang <shandbr1996@gmail.com>
2025-10-28 17:39:56 +0900

    chore: remove TypeScript artifacts from functions

--- packages/hono-d1/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { MiddlewareHandler } from 'hono'
-
-type WithD1Init = {
-  binding?: string
-  storeKey?: string
-}
-
-export declare const withD1: (init?: WithD1Init | string) => MiddlewareHandler
-export type WithD1 = ReturnType<typeof withD1>
