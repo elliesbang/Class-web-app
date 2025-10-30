433cbe763e388c86cfaefbf9442e27963c12e91e
elliesbang <shandbr1996@gmail.com>
2025-10-30 17:57:01 +0900

    Update index.js

--- functions/api/_db/index.js
+++ functions/api/_db/index.js
@@ -1,1 +1,33 @@
-export { getDB } from '../../_db.js';
+// ✅ Cloudflare Pages + D1 완전 호환 버전
+export default class DB {
+  constructor(d1) {
+    this.d1 = d1;
+  }
+
+  async run(query, params = []) {
+    try {
+      return await this.d1.prepare(query).bind(...params).run();
+    } catch (err) {
+      console.error('DB run error:', err);
+      throw err;
+    }
+  }
+
+  async all(query, params = []) {
+    try {
+      return await this.d1.prepare(query).bind(...params).all();
+    } catch (err) {
+      console.error('DB all error:', err);
+      throw err;
+    }
+  }
+
+  async first(query, params = []) {
+    try {
+      return await this.d1.prepare(query).bind(...params).first();
+    } catch (err) {
+      console.error('DB first error:', err);
+      throw err;
+    }
+  }
+}
