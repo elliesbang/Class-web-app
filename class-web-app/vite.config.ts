9a05ce5d2fb4e5cc726b9e7a671ed7cf31caf323
elliesbang <shandbr1996@gmail.com>
2025-11-14 21:52:45 +0900

    vite.config.ts 업데이트

--- class-web-app/vite.config.ts
+++ class-web-app/vite.config.ts
@@ -1,24 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+build: {
+  outDir: 'dist',   // ← ★ 이게 가장 중요함
+  emptyOutDir: true,
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  rollupOptions: {
+    output: {
+      entryFileNames: `assets/[name].[hash].js`,
+      chunkFileNames: `assets/[name].[hash].js`,
+      assetFileNames: `assets/[name].[hash].[ext]`,
     },
   },
-  build: {
-    outDir: 'dist', // ✅ Cloudflare가 읽을 수 있는 경로
-    emptyOutDir: true,
-
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`,
-      },
-    },
-  },
-})
+},
