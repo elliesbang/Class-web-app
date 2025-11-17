520ba6bd34e7bdbbd695fbeb207c83ac979b84ce
elliesbang <shandbr1996@gmail.com>
2025-11-17 20:17:38 +0900

    Delete functions/api/class_category directory

--- functions/api/class_category/list.ts
+++
@@ -1,6 +0,0 @@
-import { jsonResponse } from '../../_utils/api';
-
-export const onRequest = async ({ env }) => {
-  const result = await env.DB.prepare('SELECT * FROM class_category').all();
-  return jsonResponse(result.results ?? []);
-};
