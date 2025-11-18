d1344285500752f0aa6a3aaa0eb3a13d675c79b2
elliesbang <shandbr1996@gmail.com>
2025-11-18 12:49:51 +0900

    Update tabs.js

--- functions/api/classroom/tabs.js
+++ functions/api/classroom/tabs.js
@@ -2,7 +2,6 @@ export async function onRequest() {
   const tabs = [
     { key: 'globalNotice', label: '전체 공지' },
     { key: 'classroomVideo', label: '강의실 영상' },
-    { key: 'vodVideo', label: 'VOD 영상' },
     { key: 'material', label: '자료' },
     { key: 'classroomNotice', label: '강의실 공지' },
   ];
