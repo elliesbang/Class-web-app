4743155629681868a097ae9ac1f161f0dea9afa6
elliesbang <shandbr1996@gmail.com>
2025-11-29 15:50:22 +0900

    Update supabase imports to supabaseClient

--- app/src/pages/admin/Settings/AdminAccountSettings.tsx
+++ app/src/pages/admin/Settings/AdminAccountSettings.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabaseClient';
 
 type StatusMessage = {
   type: 'success' | 'error';
